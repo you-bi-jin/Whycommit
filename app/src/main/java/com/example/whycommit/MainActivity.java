cbeb33860c7d976dbb5803da25882766a9e59839
you-bi-jin <jinyubi3914@github.com>
2016-01-23 23:05:28 +0900

    테스트1 넣을게용~

--- app/src/main/java/com/example/whycommit/MainActivity.java
+++ app/src/main/java/com/example/whycommit/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+   String abc="테스트1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
